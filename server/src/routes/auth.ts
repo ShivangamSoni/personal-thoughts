d99f349480e4a1ef1edc13bdf1ef1543f81aebdb
Shivangam Soni <shivangamsoni99@gmail.com>
2023-02-08 18:03:52 +0530

    Add: Login API

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
 import User from "../models/User";
-import { hashPassword } from "../utils/password";
+
+import { hashPassword, validatePassword } from "../utils/password";
+import { issueJwt } from "../utils/jwt";
 
 const router = Router();
 
@@ -37,6 +39,38 @@ router.post("/register", async (req, res, next) => {
     } catch (e) {
         res.send({ success: false, message: "Invalid Email or Password" });
     }
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || email.trim() === "" || !password || password.trim() === "") {
+        return res
+            .status(400)
+            .send({ success: false, message: "Invalid Data" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).send({
+                success: false,
+                message: "Incorrect Email or Password",
+            });
+        }
+
+        const isVerified = validatePassword(password, user.salt, user.hash);
+        if (!isVerified) {
+            return res.status(400).send({
+                success: false,
+                message: "Incorrect Email or Password",
+            });
+        }
+
+        const token = issueJwt(user.id);
+        res.status(201).send({ success: true, token });
+    } catch {}
 });
 
 export const authRouter = router;
